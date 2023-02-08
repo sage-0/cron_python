e8d3933ad1cfa11877a2100cf666f10f82094147
sage <sage>
2023-02-08 22:58:04 +0900

    [add]安全のため.envファイルにトークンを移した

--- server_check.py
+++ server_check.py
@@ -2,16 +2,18 @@ import subprocess
 import requests
 from discordwebhook import Discord
 import time
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 hosts = ["192.168.0.201", "192.168.0.202"]
 
-
 def notify(host):
     payload = {'message':'\n'+f'{host}'}  
-    headers = {'Authorization': 'Bearer ' + 'pWuEO27O8VdZ2mdogIVjxfSTVbEjf2EPrKBEKYcSEVV' }
+    headers = {'Authorization': 'Bearer ' + str(os.getenv('line_token')) }
     requests.post('https://notify-api.line.me/api/notify', data=payload, headers=headers)
 
-    discord = Discord(url="https://discord.com/api/webhooks/1044980421588426833/J6_9t0abQdgAYweP8yX_gzEP7aTKDRbNNx5ZR1azp3Mcw6TrN-3-XbxnLSIYz-pTNzaz")
+    discord = Discord(url=os.getenv('discord_url'))
     discord.post(content=f"<@&1044982707144368180>\n{host}が死んでいます")
 
 def check_server(hosts):
@@ -22,7 +24,7 @@ def check_server(hosts):
         diedLists.append(
             True) if res.returncode == 0 else diedLists.append(False)
     return diedLists
-        
+
 
 
 
