e9754da0e8b1f3e07a7e8e8636ff53b15004542f
sage <sage.jp>
2023-03-10 11:26:13 +0900

    [fix]修正

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
-from crontab import crontab
+from cronjob import cronjob
 
 app = FastAPI()
 
@@ -11,8 +11,8 @@ def read_root():
 @app.post("/swich/{switch}")
 def read_item(switch: Union[str, int]):
     if switch == "on":
-        crontab(switch[0])
+        cronjob(switch[0])
         return {"statuscode": "400"}
     elif switch == "off":
-        crontab(switch[0])
+        cronjob(switch[0])
         return {"statuscode": "400"}
