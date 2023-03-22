eee121a6c6f358e9f4390bf3f8a75a44f37fb629
sage <sage>
2023-03-22 11:39:04 +0900

    [fix]cron_jobが機能しない問題を解決

--- cronjob.py
+++ cronjob.py
@@ -1,30 +1,44 @@
 from crontab import CronTab
 
 
-def get_cron_job():
-    cron = CronTab(user=True)
-    for job in cron:
-        if job.command == 'sh /home/sage/cron_python/cronjob.sh':
-            return job
-    return None
+# def get_cron_job():
+#     cron = CronTab(user=True)
+#     for job in cron:
+#         if job.command == 'sh /home/sage/cron_python/cronjob.sh':
+#             return job
+#     return None
 
 
 def cron_status():
-    job = get_cron_job()
-    if job:
-        return job.is_enabled()
-    return "error: job not found"
-
-
-def cron_job(api: str):
-    job = get_cron_job()
-    if not job:
-        return "error: job not found"
-    if api == "on":
-        job.enable(True)
-    elif api == "off" and job.is_enabled():
-        job.enable(False)
-
     cron = CronTab(user=True)
-    cron.write()
-    return "success"
+    for job in cron:
+        if job.command == 'sh /home/sage/cron_python/cronjob.sh':
+            if job:
+                return job.is_enabled()
+        else:
+            return "error: job not found"
+
+
+def cron_job(api):
+    cron = CronTab(user=True)
+    for job in cron:
+        if job.command == 'sh /home/sage/cron_python/cronjob.sh':
+            if not job:
+                return "error: job not found"
+            if api == "on":
+                if not job.is_enabled():
+                    job.enable(True)
+                    cron.write()
+                    return "success: job enabled"
+                else:
+                    return "job already enabled"
+            
+            elif api == "off":
+                if job.is_enabled():
+                    job.enable(False)
+                    cron.write()
+                    return "success: job disabled"
+                else:
+                    return "job already disabled"
+            else:
+                "error: invalid input"
